106c529ce99609fdd7fd71e37d74831f5e78e517
Daisuke Fujiwara <daisuke@nextdoor.com>
2015-05-03 18:47:31 -0700

    Add a simple table to the watch app.

--- Picker WatchKit Extension/InterfaceController.swift
+++ Picker WatchKit Extension/InterfaceController.swift
@@ -12,17 +12,30 @@ import PickerModel
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet weak var tableView: WKInterfaceTable!
+    
     override func contextForSegueWithIdentifier(segueIdentifier: String) -> AnyObject? {
         if segueIdentifier != "PickerSelection" {
             return nil
         }
-        
+
         let pickerModelStore = PickerModelStore()
         let (index, pickerModel) = pickerModelStore.randomSelect()
         if let index = index, model = pickerModel {
             return model.label
         } else {
             return nil
+        }
+    }
+
+    override func awakeWithContext(context: AnyObject?) {
+        let pickerModelStore = PickerModelStore()
+        let elements = pickerModelStore.elements
+        tableView.setNumberOfRows(elements.count, withRowType: "PickerRowType")
+
+        for (i, element) in enumerate(elements) {
+            let controller = tableView.rowControllerAtIndex(i) as! PickerRowController
+            controller.textLabel.setText(element.label)
         }
     }
 }
---
+++ Picker WatchKit Extension/PickerRowController.swift
@@ -0,0 +1,13 @@
+//
+//  PickerRowController.swift
+//  Picker
+//
+//  Created by Daisuke Fujiwara on 5/3/15.
+//  Copyright (c) 2015 daisuke.com. All rights reserved.
+//
+
+import WatchKit
+
+class PickerRowController: NSObject {
+    @IBOutlet weak var textLabel: WKInterfaceLabel!
+}
